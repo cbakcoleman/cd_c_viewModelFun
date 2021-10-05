ac6ba8c81bd730dc2c3b6fb2ba72e2194393ea3d
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-04 20:19:53 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cd_c_viewModelFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
